3fdab7fee7c4ba6a11aeaf48d7ad61c7d1ed1f68
huangyede <691715912@qq.com>
2017-12-24 23:00:39 +0800

    自动生成

--- src/main/java/com/yede/multiple/controller/UserController.java
+++ src/main/java/com/yede/multiple/controller/UserController.java
@@ -2,7 +2,6 @@ package com.yede.multiple.controller;
 
 import com.yede.multiple.mapper.UserInfoMapper;
 import com.yede.multiple.model.User;
-import com.yede.multiple.utils.DatabaseContextHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +18,7 @@ public class UserController {
     //获取指定考试安排 指定学生的考试记录
     @RequestMapping(value="", method= RequestMethod.GET)
     public List<User> selectAll() {
-        DatabaseContextHolder.setDataSourceKey("hzwitwin");
+
         return userInfoMapper.selectAll();
     }
 }
---
+++ src/main/java/com/yede/multiple/utils/JsonMapper.java
@@ -0,0 +1,37 @@
+package com.yede.multiple.utils;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+public class JsonMapper {
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+
+    public static <T> String obj2String(T src) {
+        if (src == null) {
+            return null;
+        }
+        try {
+            return src instanceof String ? (String) src : objectMapper.writeValueAsString(src);
+        } catch (Exception e) {
+            log.warn("parse object to String exception, error:{}", e);
+            return null;
+        }
+    }
+
+    public static <T> T string2Obj(String src, TypeReference<T> typeReference) {
+        if (src == null || typeReference == null) {
+            return null;
+        }
+        try {
+            return (T) (typeReference.getType().equals(String.class) ? src : objectMapper.readValue(src, typeReference));
+        } catch (Exception e) {
+            log.warn("parse String to Object exception, String:{}, TypeReference<T>:{}, error:{}", src, typeReference.getType(), e);
+            return null;
+        }
+    }
+}
